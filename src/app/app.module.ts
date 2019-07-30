5e5110d626f31ab685ec5eeedd264b36fb40c83b
Kiyotaka <t10kiyotaka@gmail.com>
2019-07-30 23:30:13 +0900

    Add Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {BasicHightlightDirective} from './basic-hightlight/basic-hightlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHightlightDirective
   ],
   imports: [
     BrowserModule,
